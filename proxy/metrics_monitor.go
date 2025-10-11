9310e46c09b266b1aeb10240b3615a7374f80fdb
Oliver <oliver@LM4.eu>
2025-10-11 13:11:54 +0200

    fork mostlygeek -> LM4eu

--- proxy/metrics_monitor.go
+++ proxy/metrics_monitor.go
@@ -5,8 +5,8 @@ import (
 	"sync"
 	"time"
 
-	"github.com/mostlygeek/llama-swap/event"
-	"github.com/mostlygeek/llama-swap/proxy/config"
+	"github.com/LM4eu/llama-swap/event"
+	"github.com/LM4eu/llama-swap/proxy/config"
 )
 
 // TokenMetrics represents parsed token statistics from llama-server logs
