939ea8c1a49428a593e337769ca1465ba3a6ffd5
Oliver <oliver@LM4.eu>
2026-01-02 19:26:02 +0100

    replace "gopkg.in/yaml.v3" -> "go.yaml.in/yaml/v4"

--- proxy/config/peer_test.go
+++ proxy/config/peer_test.go
@@ -3,7 +3,7 @@ package config
 import (
 	"testing"
 
-	"gopkg.in/yaml.v3"
+	"go.yaml.in/yaml/v4"
 )
 
 func TestPeerConfig_UnmarshalYAML(t *testing.T) {
--- proxy/helpers_test.go
+++ proxy/helpers_test.go
@@ -10,7 +10,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/LynxAIeu/llama-swap/proxy/config"
-	"gopkg.in/yaml.v3"
+	"go.yaml.in/yaml/v4"
 )
 
 var (
