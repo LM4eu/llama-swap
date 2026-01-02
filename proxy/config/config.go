2c0b77ab87fa42bfe47becff46a994f9fd84afca
Oliver <oliver@LM4.eu>
2026-01-02 19:26:01 +0100

    config: add missing MacroList.MarshalYAML()

--- proxy/config/config.go
+++ proxy/config/config.go
@@ -645,3 +645,9 @@ func substituteMacroInValue(value any, macroName string, macroValue any) (any, e
 		return value, nil
 	}
 }
+
+// MarshalYAML cannot guarantee the order because it returns map[string]any
+func (ml MacroList) MarshalYAML() (any, error) {
+	return ml.ToMap(), nil
+}
+
